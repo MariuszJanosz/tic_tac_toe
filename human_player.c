a4a628ced4068eb0134ec25ebd1acf10dbd1d546
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-02-24 12:51:52 +0100

    Refactor menu.c and human_player.c to use new get_num function

--- human_player.c
+++ human_player.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "human_player.h"
 #include "board.h"
+#include "input.h"
 
 int human_get_move(Board_t* board) {
-	int move;
+	const char *msg = "Enter selected cell 1-9: ";
+	const char *invalid_input_msg = "Invalid move! Try again.\n";
 	while (1) {
-		printf("Enter selected cell 1-9: ");
-		int scanned = scanf("%d", &move);
-		if (scanned == 0) {
-			printf("Invalid move! Try again.\n");
-			while (getchar() != '\n');
-			continue;
-		}
+		int move = get_num(msg, invalid_input_msg);
 		if (move < 1 || move > 9 ||
 			board->board[(move - 1) / 3][(move - 1) % 3] != EMPTY) {
-			printf("Invalid move! Try again.\n");
+			printf(invalid_input_msg);
 			continue;
 		}
-		while (getchar() != '\n');
 		return move;
 	}
+	assert(0);
 }
--- menu.c
+++ menu.c
@@ -3,28 +3,20 @@
 #include <assert.h>
 
 #include "menu.h"
-
-void print_start_screen();
-void print_ai_difficulty_selection_screen();
+#include "input.h"
 
 Option_t start_menu() {
-	do {
-		print_start_screen();
-		int opt;
-		int scanned = scanf("%d", &opt);
-		if (scanned == 0) {
-			char ch;
-			while ((ch = getchar()) != '\n');
-			printf("Invalid option! Try again.\n");
-			continue;
-		}
+	const char *msg =	"1. Two players\n"
+						"2. Vs AI\n"
+						"3. Quit\n"
+						"Select game mode: ";
+	const char *invalid_input_msg = "Invalid option! Try again.\n";
+	while(1) {
+		int opt = get_num(msg, invalid_input_msg);
 		if (opt < 1 || opt > 3) {
-			char ch;
-			while ((ch = getchar()) != '\n');
-			printf("Invalid option! Try again.\n");
+			printf(invalid_input_msg);
 			continue;
 		}
-		while (getchar() != '\n');
 		switch (opt) {
 		case 1:
 			return TWO_PLAYERS;
@@ -33,28 +25,22 @@ Option_t start_menu() {
 		case 3:
 			return QUIT;
 		}
-	} while (1);
+	}
 	assert(0);
 }
 
 Option_t select_ai_difficulty() {
-	do {
-		print_ai_difficulty_selection_screen();
-		int opt;
-		int scanned = scanf("%d", &opt);
-		if (scanned == 0) {
-			char ch;
-			while ((ch = getchar()) != '\n');
-			printf("Invalid option! Try again.\n");
-			continue;
-		}
+	const char *msg =	"1. Easy\n"
+						"2. Hard\n"
+						"3. Perfect\n"
+						"Select AI difficulty: ";
+	const char *invalid_input_msg = "Invalid option! Try again.\n";
+	while (1) {
+		int opt = get_num(msg, invalid_input_msg);
 		if (opt < 1 || opt > 3) {
-			char ch;
-			while ((ch = getchar()) != '\n');
-			printf("Invalid option! Try again.\n");
+			printf(invalid_input_msg);
 			continue;
 		}
-		while (getchar() != '\n');
 		switch (opt) {
 		case 1:
 			return PLAYER_VS_AI_STUPID;
@@ -63,20 +49,6 @@ Option_t select_ai_difficulty() {
 		case 3:
 			return PLAYER_VS_AI_PERFECT;
 		}
-	} while (1);
+	}
 	assert(0);
-}
-
-void print_start_screen() {
-	printf("1. Two players\n");
-	printf("2. Vs AI\n");
-	printf("3. Quit\n");
-	printf("Select game mode: ");
-}
-
-void print_ai_difficulty_selection_screen() {
-	printf("1. Easy\n");
-	printf("2. Hard\n");
-	printf("3. Perfect\n");
-	printf("Select AI difficulty: ");
 }
