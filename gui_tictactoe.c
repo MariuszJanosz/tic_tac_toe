df16312e1e27dd298f65cc6f18e07558f6d35142
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-03-07 08:49:44 +0100

    Add inintializing of glfw and creating empty window

--- gui_tictactoe.c
+++ gui_tictactoe.c
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "GLFW/glfw3.h"
 #include "board.h"
 #include "players.h"
 #include "gui_tictactoe.h"
@@ -6,7 +10,37 @@ void gui_init_game(Board_t* board, Player_t* players) {
 	//TODO
 }
 
+void glfw_window_init(GLFWwindow** window, int width, int height, const char* name) {
+    if (!glfwInit()) {
+        fprintf(stderr, "GLFW initialization failed!");
+        exit(1);
+    }
+    *window = glfwCreateWindow(width, height, name, NULL, NULL);
+    if (!*window)
+    {
+        glfwTerminate();
+        fprintf(stderr, "GLFW window creation failed!");
+        exit(1);
+    }
+    glfwMakeContextCurrent(*window);
+}
+
 int gui_main(int atgc, char** argv) {
-	//TODO
-	return 0;
+    GLFWwindow* window = NULL;
+    glfw_window_init(&window, 800, 800, "TicTacToe");
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
