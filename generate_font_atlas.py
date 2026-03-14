0b8de56d563891a339a0e0668b2a5c56c755df28
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-03-14 08:12:51 +0100

    Add width height and channels to font_atlas_data_t and make generate_font_atlas.py to work with any size of input

--- generate_font_atlas.py
+++ generate_font_atlas.py
@@ -1,35 +1,62 @@
 import os
 import sys
 import io
+import re
 import imageio.v3 as iio
 
-def write_data_line(out_file, image, i):
-	line = img[i]
-	for j in range(256):
-		out_file.write(str(line[j][0]))
-		out_file.write(',')
-		out_file.write(str(line[j][1]))
-		out_file.write(',')
-		out_file.write(str(line[j][2]))
-		out_file.write(',')
-		out_file.write(str(line[j][3]))
-		if i != 255 or j != 255:
-			out_file.write(', ')
+def write_data_line(out_file, image, line_id, width, height, channels):
+	line = img[line_id]
+	for col in range(width):
+		for ch in range(channels):
+			out_file.write(str(line[col][ch]))
+			if line_id != (height - 1) or col != (width - 1) or ch != (channels - 1):
+				out_file.write(',')
+		out_file.write(' ')
 	out_file.write('\n')
 
 file_path = os.path.abspath(sys.argv[1])
 img_path =  os.path.abspath(sys.argv[2])
 
 f = io.open(file_path, 'w')
-img = iio.imread(img_path, mode="RGBA")
+img = iio.imread(img_path)
+width = img.shape[0]
+height = img.shape[1]
+channels = img.shape[2]
+size = width * height * channels
 
 f.write('#include "font_atlas.h"\n')
 f.write('\n')
 f.write('GLuint font_atlas_tex;\n')
-f.write('Font_atlas_data_t __font_atlas_data__ = {.RGBA = {\n')
-for i in range(256):
-	write_data_line(f, img, i)
+f.write('Font_atlas_data_t __font_atlas_data__ = {\n')
+f.write(f'.width = {width},\n')
+f.write(f'.height = {height},\n')
+f.write(f'.channels = {channels},\n')
+f.write('.RGBA = {\n')
+for line_id in range(height):
+	write_data_line(f, img, line_id, width, height, channels)
 f.write('}};\n')
 f.write('\n')
 f.write('Font_atlas_data_t* font_atlas_data = &__font_atlas_data__;\n')
 f.close()
+
+h_file_path = re.sub('.c$', '.h', file_path)
+f_h = io.open(h_file_path, 'w')
+f_h.write('#ifndef FONT_ATLAS_H\n')
+f_h.write('#define FONT_ATLAS_H\n')
+f_h.write('\n')
+f_h.write('#include "GLFW/glfw3.h"\n')
+f_h.write('\n')
+f_h.write(f'#define ATLAS_WIDTH {width}\n')
+f_h.write(f'#define ATLAS_HEIGHT {height}\n')
+f_h.write(f'#define ATLAS_CHANNELS {channels}\n')
+f_h.write('\n')
+f_h.write('typedef struct {\n')
+f_h.write('	int width, height, channels;\n')
+f_h.write('	GLubyte RGBA[ATLAS_WIDTH * ATLAS_HEIGHT * ATLAS_CHANNELS];\n')
+f_h.write('} Font_atlas_data_t;\n')
+f_h.write('\n')
+f_h.write('extern GLuint font_atlas_tex;\n')
+f_h.write('extern Font_atlas_data_t* font_atlas_data;\n')
+f_h.write('\n')
+f_h.write('#endif //FONT_ATLAS_H\n')
+f_h.close()
