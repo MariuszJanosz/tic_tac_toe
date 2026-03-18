3e1c95d985cea1effc5f7b51e7f758fd9f118770
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-03-18 08:24:18 +0100

    Add color to button: blue while hovered, green while pressed and red otherwise

--- button.c
+++ button.c
@@ -24,13 +24,31 @@ void init_button(	Button_t* button, Button_id_t button_id,
 void draw_button(Button_t* button) {
 	GLdouble	xpos = button->xpos, ypos = button->ypos,
 				w = button->width, h = button->height;
-	GLubyte r = 0, g = 0, b = 0;
-	glColor3ub(r, g, b);
+	glColor3ub(0, 0, 0);
 	glBegin(GL_LINE_LOOP);
 	glVertex2d(xpos, ypos);
 	glVertex2d(xpos + w, ypos);
 	glVertex2d(xpos + w, ypos + h);
 	glVertex2d(xpos, ypos + h);
+	glEnd();
+	Mouse_t mouse;
+	glfwGetCursorPos(glfwGetCurrentContext(), &mouse.xpos, &mouse.ypos);
+	int is_hovered =	xpos <= mouse.xpos && xpos + w >= mouse.xpos &&
+						ypos <= mouse.ypos && ypos + h >= mouse.ypos ? 1 : 0;
+	if (button->is_pressed) {
+		glColor3ub(0, 255, 0);
+	}
+	else if (is_hovered) {
+		glColor3ub(0, 0, 255);
+	}
+	else {
+		glColor3ub(255, 0, 0);
+	}
+	glBegin(GL_QUADS);
+		glVertex2d(xpos, ypos + 1);
+		glVertex2d(xpos + w - 1, ypos + 1);
+		glVertex2d(xpos + w - 1, ypos + h);
+		glVertex2d(xpos, ypos + h);
 	glEnd();
 	unsigned int size = 1;
 	gui_draw_string(button->desc, xpos + w + 10, ypos + (h - size * ATLAS_HEIGHT) / 2, size);
